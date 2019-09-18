a77621f5a3a72cb564a36fb7d60e437bd80527a5
Shishir Jha <shishirjhaccr@gmail.com>
2019-09-18 06:57:10 +0530

    added feature such that server returns json web token

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const router = express.Router();
 const User = require("../models/User");
 const { check, validationResult } = require("express-validator");
@@ -45,7 +47,22 @@ router.post(
 
       await user.save();
 
-      res.send("User saved");
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
