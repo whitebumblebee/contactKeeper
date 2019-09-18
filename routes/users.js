402c3530ab96c0b1d959e3d61a2bf0cffe89bfbd
Shishir Jha <shishirjhaccr@gmail.com>
2019-09-18 08:58:13 +0530

    added authentication

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 const express = require("express");
+const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
-const router = express.Router();
 const User = require("../models/User");
 const { check, validationResult } = require("express-validator");
 
