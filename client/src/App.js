4203bdf1fcbf7ef260c0c92500e21ebaf7641242
Shishir Jha <shishirjhaccr@gmail.com>
2019-09-25 06:39:49 +0530

    added contactContext provider to APP.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import "./App.css";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+
+import ContactState from "./context/contact/ContactState";
 
 const App = () => {
   return (
-    <div className='App'>
-      <Navbar />
-      My App
-    </div>
+    <ContactState>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
   );
 };
 
